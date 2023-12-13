5667679808d89e744907796dfa7991e5756ebe20
Eileen Lee <lee.ei@northeastern.edu>
2023-12-12 19:43:17 -0500

    update ?_?

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ import UserRoutes from "./users/routes.js";
 import cors from "cors";
 import session from "express-session";
 import mongoose from "mongoose";
-mongoose.connect("mongodb://127.0.0.1:27017/game-app");
+const CONNECTION_STRING =
+  process.env.DB_CONNECTION_STRING || "mongodb://127.0.0.1:27017/game-app";
+mongoose.connect(CONNECTION_STRING);
 
 const app = express();
 app.get("/", (req, res) => {
--- users/schema.js
+++ users/schema.js
@@ -3,8 +3,6 @@ const userSchema = new mongoose.Schema(
   {
     username: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    email: String,
-    dob: Date,
     role: {
       type: String,
       enum: ["GAMER", "DEVELOPER"],
